55c7023d5152f3ef45cb8cac41bd7e20cb05cd1f
Aromal Satheesh <aromalsatheesh802003@gmail.com>
2026-06-01 22:52:55 +0700

    static and template inheritance

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import *
+from .views import home,about,payment
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +26,8 @@ urlpatterns = [
     path('products/',include('products.urls')),
 
     path('',home), 
+    path('about/',about),
+    path('payment/',payment),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/views.py
+++ backend/views.py
@@ -7,3 +7,10 @@ def home(request):
         'products':products
     }
     return render(request,'home.html',context)
+
+
+def about(request):
+    return render(request,'about.html')
+
+def payment(request):
+    return render(request,'payment.html')
